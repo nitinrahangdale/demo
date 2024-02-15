de6d5d0f104ee9a255cab89aacee992a2589fffb
Nitin Rahangdale <nitin.rahangdale@infoorigin.com>
2024-02-15 13:40:41 +0530

    Adding ENV variables for url

--- src/app/ngServices/reportService.ts
+++ src/app/ngServices/reportService.ts
@@ -1,24 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/enviroments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
+  private baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
-
-  saveReport(reportData: any): Promise<any> {
-    return this.http.post<any>('/save-report', { reportData }).toPromise();
-  }
-
-  getReport(): Promise<any> {
-    return this.http.get<any>('/get-report').toPromise();
-  }
-
+  
   executeTest(testCase: string) {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     const body = { testCase: testCase };
-    return this.http.post<any>('http://localhost:3000/runtest', body, { headers: headers });
+    return this.http.post<any>(this.baseUrl + '/run', body, { headers: headers });
   }
 }
