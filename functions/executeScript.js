01e7721c20ad1d8e5030f5c0cc0b4b31164abbe7
Nitin Rahangdale <nitin.rahangdale@infoorigin.com>
2024-02-16 19:48:22 +0530

    Adding cors for all

--- functions/executeScript.js
+++ functions/executeScript.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 // Enable CORS middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://codingdev.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
